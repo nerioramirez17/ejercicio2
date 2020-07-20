07eee4674e9d8867bb2439127040aa7f35755ef3
nerio junior ramirez zambrano <nerioramirez17@gmail.com>
2020-07-20 16:25:10 -0300

    Initial commit

---
+++ src/app/search/search.component.ts
@@ -0,0 +1,31 @@
+import { Component, OnInit, AfterViewInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { DataService } from '../data.service';
+@Component({
+  selector: 'search-component',
+  templateUrl: './search.component.html',
+  styleUrls: ['./search.component.css']
+})
+export class SearchComponent implements OnInit {
+
+  searchForm: FormGroup;
+  constructor(private fb: FormBuilder, public service: DataService) { }
+
+  ngOnInit() {
+    // add corresponding validators
+    this.searchForm = this.fb.group({
+      'searchVegetable': [null]
+    });
+
+     // write a function that calls changeVegetableName upon value change in the form
+     this.onChanges();
+  }
+
+  onChanges(): void {
+    this.searchForm.valueChanges.subscribe(val => {
+        this.service.changeVegetableName(val);
+    });
+  }
+  
+
+}
